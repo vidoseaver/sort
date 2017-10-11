6b96657af4a329d957f842edf3225b01477363df
Vido <vidoseaver@gmail.com>
2017-10-11 12:54:49 -0600

    Add insert_sort and tests, refactor out insert_number_in_correct_position

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Sorter
 
   def initialize(unsorted_list)
@@ -9,4 +11,27 @@ class Sorter
     num_2
   end
 
+  def insert_sort
+    sorted_list = []
+    @unsorted_list.each do |number|
+      if sorted_list.first == nil
+        sorted_list.unshift(number)
+      else
+        sorted_list = insert_number_in_correct_postion(sorted_list, number)
+      end
+    end
+    sorted_list
+  end
+
+  def insert_number_in_correct_postion(list, number)
+    new_list = list
+    list.each_with_index do |list_number, index|
+      if number <= list_number
+        return new_list.insert(index, number)
+      elsif list[index + 1] == nil
+        return new_list.push(number)
+      end
+    end
+    new_list
+  end
 end
--- test/sorter_test.rb
+++ test/sorter_test.rb
@@ -10,9 +10,9 @@ RSpec.describe Sorter do
       expect(sorter).to be_a Sorter
     end
     context "instance methods" do
+      list = [4,2]
+      sorter = Sorter.new(list)
       describe "larger_number" do
-        list = [4,2]
-        sorter = Sorter.new(list)
         it "returns the larger of the two numbers" do
             expect(sorter.larger_number(4,2)).to eq 4
         end
@@ -22,6 +22,36 @@ RSpec.describe Sorter do
 
         it "it returns the number if they are equal" do
           expect(sorter.larger_number(4,4)).to eq 4
+        end
+      end
+      describe "insert_sort" do
+        it "returns an array" do
+          expect(sorter.insert_sort.class).to eq Array
+        end
+        it "it sorts the array" do
+          expect(sorter.insert_sort).to eq [2,4]
+        end
+        it "can handle arrays with repeated values" do
+          list = [6,6,6,8,8,2,1]
+          sorter = Sorter.new(list)
+          expect(sorter.insert_sort).to eq [1, 2, 6, 6, 6, 8, 8]
+        end
+      end
+      describe "insert_number_in_correct_postion" do
+        it "inserts a number greater then the number in the array after it" do
+          expect(sorter.insert_number_in_correct_postion([2],4)).to eq [2,4]
+        end
+        it "it inserts a number at the start of the array if its less than it" do
+          expect(sorter.insert_number_in_correct_postion([4],2)).to eq [2,4]
+        end
+        it "it inserts a number in the middle of a lower and higher number" do
+          expect(sorter.insert_number_in_correct_postion([2,8],4)).to eq [2,4,8]
+        end
+        it "if it is the same as the middle number it puts it befor it" do
+          expect(sorter.insert_number_in_correct_postion([2,4,8],4)).to eq [2,4,4,8]
+        end
+        it "if it is the same as the first number it can handle it" do
+          expect(sorter.insert_number_in_correct_postion([2],2)).to eq [2,2]
         end
       end
     end
