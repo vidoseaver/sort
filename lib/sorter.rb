7a8c99489bce13937f128ec1f040f2f509570513
Vido <vidoseaver@gmail.com>
2017-10-11 11:46:56 -0600

    Add #larger_number which returns the larger of two numbers and tests

--- lib/sorter.rb
+++ lib/sorter.rb
@@ -4,5 +4,9 @@ class Sorter
     @unsorted_list = unsorted_list
   end
 
+  def larger_number(num_1, num_2)
+    return num_1 if num_1 >= num_2
+    num_2
+  end
 
 end
--- test/sorter_test.rb
+++ test/sorter_test.rb
@@ -9,5 +9,21 @@ RSpec.describe Sorter do
       sorter = Sorter.new(list)
       expect(sorter).to be_a Sorter
     end
+    context "instance methods" do
+      describe "larger_number" do
+        list = [4,2]
+        sorter = Sorter.new(list)
+        it "returns the larger of the two numbers" do
+            expect(sorter.larger_number(4,2)).to eq 4
+        end
+        it "works if the numbers are reversed" do
+          expect(sorter.larger_number(2,4)).to eq 4
+        end
+
+        it "it returns the number if they are equal" do
+          expect(sorter.larger_number(4,4)).to eq 4
+        end
+      end
+    end
   end
 end
