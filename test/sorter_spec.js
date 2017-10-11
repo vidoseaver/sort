872776f7649e42690a243575eb7cc561d022daf3
Vido <vidoseaver@gmail.com>
2017-10-11 14:07:14 -0600

    Add sorter.js and sorter_spec.js and make sure they are wired up

---
+++ test/sorter_spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+var Sorter = require('../lib/sorter.js');
+
+describe('Sorter', function() {
+    it('should exist', function() {
+        expect(Sorter).to.not.be.undefined;
+    });
+});
